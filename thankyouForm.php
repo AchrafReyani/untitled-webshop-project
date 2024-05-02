508afecf1746b74f8e2b53ac499e52c661735af5
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-02 15:43:21 +0200

    (#4)

--- thankyouForm.php
+++
@@ -1,6 +0,0 @@
-<?php
-function showThankYouFormPage() {
-  echo "<h2>Thank you!</h2>";
-  echo "<p>Your message has been sent. I will get back to you as soon as possible.</p>";
-}
-?>
--- thankyouOrder.php
+++
@@ -1,7 +0,0 @@
-<?php
-function showThankYouOrderPage() {
-  echo "<h2>Thank you!</h2>";
-  echo "<p>Thank you for your order!</p>";
-  echo "<p>Your support of our business means the world.</p>";
-}
-?>
