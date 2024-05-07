1a5aec9848a539c10eb2b4468e5825fd8b0d07b0
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-07 15:15:44 +0200

    contact form displays no errors on test file

--- tests/test_contact_doc.php
+++ tests/test_contact_doc.php
@@ -2,6 +2,26 @@
 include_once "../views/ContactDoc.php";
 $data = array ('page' => 'Contact');
 $data['menu'] = array('Home' => 'Home', 'About' => 'About', 'Contact' => 'Contact', 'Webshop' => 'Webshop');
+$data['pronouns'] = "";
+$data['pronounsError'] = "";
+$data['name'] = "";
+$data['nameError'] = "";
+$data['email'] = "";
+$data['emailError'] = "";
+$data['phonenumber'] = "";
+$data['phonenumberError'] = "";
+$data['street'] = "";
+$data['streetError'] = "";
+$data['housenumber'] = "";
+$data['housenumberError'] = "";
+$data['postalcode'] = "";
+$data['postalcodeError'] = "";
+$data['city'] = "";
+$data['cityError'] = "";
+$data['communication'] = "";
+$data['communicationError'] = "";
+$data['message'] = "";
+$data['messageError'] = "";
 $view = new ContactDoc($data);
 $view -> show();
 ?>
