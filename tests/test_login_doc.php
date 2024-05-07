0891141fe867a041c05a50e5f01fd3a9a9f692b7
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-07 12:33:45 +0200

    (#3) (#4)

--- tests/test_login_doc.php
+++ tests/test_login_doc.php
@@ -2,6 +2,11 @@
 include_once "../views/LoginDoc.php";
 $data = array ('page' => 'Login');
 $data['menu'] = array('Home' => 'Home', 'About' => 'About', 'Contact' => 'Contact', 'Webshop' => 'Webshop');
+$data['emailError'] = "";
+$data['email'] = "";
+$data['password'] = "";
+$data['passwordError'] = "";
+$data['loginError'] = "";
 $view = new LoginDoc($data);
 $view -> show();
 ?>
--- tests/test_register_doc.php
+++ tests/test_register_doc.php
@@ -2,6 +2,14 @@
 include_once "../views/RegisterDoc.php";
 $data = array ('page' => 'Register');
 $data['menu'] = array('Home' => 'Home', 'About' => 'About', 'Contact' => 'Contact', 'Webshop' => 'Webshop');
+$data['emailError'] = "";
+$data['email'] = "";
+$data['password'] = "";
+$data['confirm_passwordError'] = "";
+$data['confirm_password'] = "";
+$data['passwordError'] = "";
+$data['nameError'] = "";
+$data['name'] = "";
 $view = new RegisterDoc($data);
 $view -> show();
 ?>
