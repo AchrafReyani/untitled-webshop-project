9029850ce846cc90f9f1d97b61cefec82440f95e
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-15 17:39:23 +0200

    (#8)

--- models/ShopModel.php
+++ models/ShopModel.php
@@ -1,7 +1,64 @@
 <?php //this class interacts with the database
 class ShopModel extends PageModel {
+
+    public $products = [];
+
     public function __construct($pageModel) {
         PARENT::__construct($pageModel);
     }
+
+
+    public function getWebShopProduct() {
+        $product = NULL;
+        $generalError = "";
+      
+        try {
+          //display nothing (id=0) when no id has been set
+          $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+          if ($id != 0) {
+            include_once 'db.php';
+            $product = getProductById($id);
+          }
+          if(empty($product)) {
+            $generalError = "The product with id: ".$id." was not found.";
+          }
+        } catch (Exception $e) {
+            $generalError = "A technical error ocurred, please come back later";
+            logError("GetWebshopProductFailed: " . $e->getMessage());
+        }
+        return ['product' => $product, 'generalError' => $generalError];
+      }
+      
+     public function getWebshopProducts() {
+        include_once 'db.php';
+        $products = getAllProducts();
+        echo is_null($products);
+        return ['products' => $products];
+      }
+      
+      public function handleCartActions() {
+        if (isset($_POST['action'])) {
+          $action = $_POST['action'];
+        } else {
+          $action = "";
+        }
+        switch ($action)
+        {
+          case 'addToShoppingCart';
+            $id = $_POST['id'];
+            addToShoppingCart($id);
+            break;
+          case 'removeFromShoppingCart';
+            $id = $_POST['id'];
+            removeFromShoppingCart($id);
+            break;
+          case 'submitShoppingCart'; 
+            placeOrder();
+            deleteShoppingCart();//TODO maybe make seperate function for emptying thhe shopping cart and completely unsetting it for logging out
+            makeShoppingCart();
+            break;
+        }
+        return $action;
+      }
 }
 ?>
