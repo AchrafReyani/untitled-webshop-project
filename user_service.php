399136a82502e7a38945875f9c3ee5769a1140b7
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-23 15:00:54 +0200

    added comment to user service to address it's not being used atm

--- user_service.php
+++ user_service.php
@@ -7,6 +7,7 @@ function authenticateUserById($id, $password) {
     //TODO (#11)
 }
 
+//is not used
 function storeUser($email, $name, $password) {
   //also hashes the password at the moment, could also be a seperate function
   $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
