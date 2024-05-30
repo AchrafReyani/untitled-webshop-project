1c4236a2c29d2f87c39940da91b44d084f54e9a3
Achraf Reyani <achrafreyani99@gmail.com>
2024-05-30 12:08:02 +0200

    testing github actions

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'Crud.php';
 require_once 'CrudFactory.php';
 require_once 'ModelFactory.php';
 require_once 'controllers/PageController.php';
-
+//test
 session_start();
 
 $crud = new Crud();
